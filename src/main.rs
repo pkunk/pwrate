0de2243de7e8669374e7c341fcba7c11604976c0
Alex B <pkunk.ab@gmail.com>
2022-07-09 01:01:37 +0300

    implement editing of allowed-rates

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashSet;
 use std::ffi::OsStr;
 use std::fs::File;
 use std::io::Write;
@@ -8,7 +9,7 @@ use std::{env, fs};
 
 use gtk::glib::clone;
 use gtk::prelude::*;
-use gtk::{Align, Application, ApplicationWindow, Button, ComboBoxText};
+use gtk::{Align, Application, ApplicationWindow, Button, CheckButton, ComboBoxText, Label};
 use once_cell::sync::OnceCell;
 use serde::{Deserialize, Serialize};
 
@@ -20,14 +21,14 @@ struct PwContextProperties {
     #[serde(rename = "default.clock.rate")]
     rate: u32,
     #[serde(rename = "default.clock.allowed-rates")]
-    allowed_rates: Vec<u32>,
+    allowed_rates: HashSet<u32>,
 }
 
 impl Default for PwContextProperties {
     fn default() -> Self {
         Self {
             rate: 48000,
-            allowed_rates: vec![48000],
+            allowed_rates: HashSet::from_iter(vec![48000].into_iter()),
         }
     }
 }
@@ -72,6 +73,13 @@ fn build_ui(application: &Application) {
         .build();
 
     let container = gtk::Box::builder()
+        .orientation(gtk::Orientation::Vertical)
+        .halign(Align::Center)
+        .valign(Align::Center)
+        .margin_bottom(10)
+        .build();
+
+    let container_rate = gtk::Box::builder()
         .orientation(gtk::Orientation::Horizontal)
         .margin_top(10)
         .margin_bottom(10)
@@ -81,6 +89,10 @@ fn build_ui(application: &Application) {
         .valign(Align::Center)
         .spacing(10)
         .build();
+
+    let lbl_rate = Label::builder().label("Default rate:").build();
+
+    container_rate.append(&lbl_rate);
 
     let cmb_rate = ComboBoxText::builder()
         .margin_top(10)
@@ -102,12 +114,13 @@ fn build_ui(application: &Application) {
     cmb_rate.connect_changed(clone!(@weak cmb_rate => move |c| {
         if let Some(rate) = c.active_text() {
             if let Ok(rate) = rate.to_string().parse::<u32>() {
-                set_rate(rate);
+                conf().lock().unwrap().properties.rate = rate;
+                set_rate();
             }
         }
     }));
 
-    container.append(&cmb_rate);
+    container_rate.append(&cmb_rate);
 
     let btn_save = Button::builder()
         .label("Save Config")
@@ -125,28 +138,124 @@ fn build_ui(application: &Application) {
         });
     });
 
-    container.append(&btn_save);
+    container_rate.append(&btn_save);
+
+    container.append(&container_rate);
+
+    let lbl_rates = Label::builder().label("Allowed rates:").build();
+    let rates = &conf().lock().unwrap().properties.allowed_rates;
+
+    let chb_44100 = CheckButton::builder()
+        .label("  44100")
+        .active(rates.contains(&44100))
+        .margin_start(10)
+        .build();
+    chb_44100.connect_toggled(clone!(@weak chb_44100 => move |c| {
+        {
+            let rates = &mut conf().lock().unwrap().properties.allowed_rates;
+            if c.is_active() {
+                rates.insert(44100);
+            } else {
+                rates.remove(&44100);
+            }
+        }
+        set_rates();
+    }));
+
+    let chb_48000 = CheckButton::builder()
+        .label("  48000")
+        .active(rates.contains(&48000))
+        .margin_start(10)
+        .build();
+    chb_48000.connect_toggled(clone!(@weak chb_48000 => move |c| {
+        {
+            let rates = &mut conf().lock().unwrap().properties.allowed_rates;
+            if c.is_active() {
+                rates.insert(48000);
+            } else {
+                rates.remove(&48000);
+            }
+        }
+        set_rates();
+    }));
+
+    let chb_88200 = CheckButton::builder()
+        .label("  88200")
+        .active(rates.contains(&88200))
+        .margin_start(10)
+        .build();
+    chb_88200.connect_toggled(clone!(@weak chb_88200 => move |c| {
+        {
+            let rates = &mut conf().lock().unwrap().properties.allowed_rates;
+            if c.is_active() {
+                rates.insert(88200);
+            } else {
+                rates.remove(&88200);
+            }
+        }
+        set_rates();
+    }));
+
+    let chb_96000 = CheckButton::builder()
+        .label("  96000")
+        .active(rates.contains(&96000))
+        .margin_start(10)
+        .build();
+    chb_96000.connect_toggled(clone!(@weak chb_96000 => move |c| {
+        {
+            let rates = &mut conf().lock().unwrap().properties.allowed_rates;
+            if c.is_active() {
+                rates.insert(96000);
+            } else {
+                rates.remove(&96000);
+            }
+        }
+        set_rates();
+    }));
+
+    let chb_192000 = CheckButton::builder()
+        .label(" 192000")
+        .active(rates.contains(&192000))
+        .margin_start(10)
+        .build();
+    chb_192000.connect_toggled(clone!(@weak chb_192000 => move |c| {
+        {
+            let rates = &mut conf().lock().unwrap().properties.allowed_rates;
+            if c.is_active() {
+                rates.insert(192000);
+            } else {
+                rates.remove(&192000);
+            }
+        }
+        set_rates();
+    }));
+
+    container.append(&lbl_rates);
+    container.append(&chb_44100);
+    container.append(&chb_48000);
+    container.append(&chb_88200);
+    container.append(&chb_96000);
+    container.append(&chb_192000);
 
     window.set_child(Some(&container));
 
     window.show();
 }
 
-fn set_rate(rate: u32) {
-    let rates = vec![rate];
-    conf().lock().unwrap().properties.rate = rate;
-    conf().lock().unwrap().properties.allowed_rates = rates.clone();
-    //TODO: replace with native implementation
-    pw_metadata(
-        "clock.allowed-rates",
-        serde_json::to_string(&rates).unwrap(),
-    );
+fn set_rate() {
+    let rate = conf().lock().unwrap().properties.rate;
     pw_metadata("clock.rate", rate.to_string());
     pw_metadata("clock.force-rate", rate.to_string());
     pw_metadata("-d", "clock.force-rate");
 }
 
+fn set_rates() {
+    let rates = &conf().lock().unwrap().properties.allowed_rates;
+    pw_metadata("clock.allowed-rates", serde_json::to_string(rates).unwrap());
+}
+
 fn pw_metadata<K: AsRef<OsStr>, V: AsRef<OsStr>>(key: K, value: V) {
+    //TODO: replace with native implementation
     let _ = Command::new("pw-metadata")
         .stdin(Stdio::null())
         .stdout(Stdio::null())
